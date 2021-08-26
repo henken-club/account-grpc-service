02824b10e1886b626f4e2824e45994b8972efeaf
SnO2WMaN <me@sno2wman.net>
2021-08-26 21:19:26 +0900

    fix prisma delete

--- src/test/prisma.utils.ts
+++ src/test/prisma.utils.ts
@@ -1,16 +1,12 @@
-import {Prisma, PrismaClient} from '@prisma/client';
+import {PrismaClient} from '@prisma/client';
 
 export const cleanPrisma = async (): Promise<void> => {
   const prisma = new PrismaClient();
-
-  const transactions = Prisma.dmmf.datamodel.models
-    .map((model) => model.name)
-    .map((modelName) =>
-      (prisma as any)[
-        modelName.charAt(0).toLowerCase() + modelName.slice(1)
-      ].deleteMany(),
-    );
-
-  await prisma.$transaction(transactions);
+  await prisma.$transaction([
+    prisma.registration.deleteMany(),
+    prisma.temporaryUser.deleteMany(),
+    prisma.setting.deleteMany(),
+    prisma.user.deleteMany(),
+  ]);
   await prisma.$disconnect();
 };
